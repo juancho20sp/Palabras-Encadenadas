2a02b5e2a6a351c76d7452dd19991c23a87c9d5b
juancho20sp <juancho20sp@gmail.com>
2020-04-07 16:01:44 -0500

    Views done!

--- files/views/views.py
+++ files/views/views.py
@@ -32,7 +32,7 @@ class PalabrasEncadenadas(tk.Tk):
         self.frames = {}
 
         # Fill the dictionary with the frames
-        for view in (StartPage, GameMode, UsrRegisteredSelection, UsrRegistered, UsrNotRegistered):
+        for view in (StartPage, GameMode, UsrRegisteredSelection, UsrRegistered, UsrNotRegistered, MenuData, ThemeData, PlayerData):
             # Pass the container to the frame
             frame = view(container, self)
 
@@ -64,7 +64,11 @@ class PalabrasEncadenadas(tk.Tk):
         """
         self.destroy()
 
+
 class StartPage(tk.Frame):
+    """
+    Clase que crea los elementos del menú principal del juego.
+    """
     def __init__(self, parent, controller):
         """
         Constructor de la página inicial del juego.
@@ -83,13 +87,17 @@ class StartPage(tk.Frame):
         play_btn = ttk.Button(self, text="¡A jugar!", command=lambda: controller.show_frame(GameMode))
         play_btn.pack(pady=35)
 
-        data_btn = ttk.Button(self, text="Ver Datos")
+        data_btn = ttk.Button(self, text="Ver Datos", command=lambda: controller.show_frame(MenuData))
         data_btn.pack()
 
         exit_btn = ttk.Button(self, text="Salir", command=controller.end_game)
         exit_btn.pack(pady=35)
 
+
 class GameMode(tk.Frame):
+    """
+    Clase que crea los elementos del menú de selección de modo del juego.
+    """
     def __init__(self, parent, controller):
         """
         Constructor de la página de 'selección de modo' del juego.
@@ -158,8 +166,17 @@ class GameMode(tk.Frame):
         """
         print(theme)
 
+
 class UsrRegisteredSelection(tk.Frame):
+    """
+    Clase que crea los elementos del menú de deciciones respecto al registro de usuarios del juego.
+    """
     def __init__(self, parent, controller):
+        """
+        Constructor de la página de 'selección de regustro de usuarios' del juego.
+        :param parent: Clase de la que hereda el contenedor.
+        :param controller: Clase de la que hereda las funciones de control.
+        """
         tk.Frame.__init__(self, parent)
 
         # Variables
@@ -193,36 +210,264 @@ class UsrRegisteredSelection(tk.Frame):
         back_btn = ttk.Button(buttons_frame, text="Volver", command=lambda: controller.show_frame(GameMode))
         back_btn.pack(ipadx=30)
 
-
-
-
-
-
-
 class UsrRegistered(tk.Frame):
+    """
+    Clase que crea los elementos del menú de deciciones respecto al registro de usuarios del juego.
+    """
     def __init__(self, parent, controller):
+        """
+        Constructor de las vistas de ingreso de un usuario registrado al juego.
+        :param parent: Clase de la que hereda el contenedor.
+        :param controller: Clase de la que hereda las funciones de control.
+        """
         tk.Frame.__init__(self, parent)
 
         # Title
-        title = ttk.Label(self, text="USER REGISTERED")
+        title = ttk.Label(self, text="¡Preparemos el juego!", font=LARGE_FONT)
         title.pack()
+
+        # Input frame
+        input_frame = tk.Frame(self)
+        input_frame.pack(pady=25)
+
+        label = ttk.Label(input_frame, text="Email o nombre de usuario: ")
+        label.pack(side="left")
+
+        entry = ttk.Entry(input_frame)
+        entry.pack(fill="x")
+
+        # Navigation buttons frame
+        buttons_frame = tk.Frame(self)
+        buttons_frame.pack(pady=35)
+
+        next_player = ttk.Button(buttons_frame, text="Siguiente jugador")
+        next_player.pack(side="left")
+
+        return_btn = ttk.Button(buttons_frame, text="Volver", command=lambda: controller.show_frame(UsrRegisteredSelection))
+        return_btn.pack(padx=10)
 
 class UsrNotRegistered(tk.Frame):
+    """
+    Clase que crea los elementos del menú de deciciones respecto al registro de usuarios del juego.
+    """
+    def __init__(self, parent, controller):
+        """
+        Constructor de las vistas de ingreso de un usuario no registrado al juego.
+        :param parent: Clase de la que hereda el contenedor.
+        :param controller: Clase de la que hereda las funciones de control.
+        """
+        tk.Frame.__init__(self, parent)
+
+        # Title
+        title = ttk.Label(self, text="¡Preparemos el juego!", font=LARGE_FONT)
+        title.pack()
+
+        # Form
+        form_frame = tk.Frame(self)
+        form_frame.pack(pady=20)
+
+        # Name
+        name_frame = tk.Frame(form_frame)
+        name_frame.pack()
+
+        name_lbl = ttk.Label(name_frame, text="Nombre:", width=25)
+        name_lbl.pack(side="left")
+
+        name_entry = ttk.Entry(name_frame)
+        name_entry.pack(fill="x")
+
+        # Last name
+        last_frame = tk.Frame(form_frame)
+        last_frame.pack(pady=10)
+
+        last_lbl = ttk.Label(last_frame, text="Apellido:", width=25)
+        last_lbl.pack(side="left")
+
+        last_entry = ttk.Entry(last_frame)
+        last_entry.pack(fill="x")
+
+        # Username
+        username_frame = tk.Frame(form_frame)
+        username_frame.pack(pady=10)
+
+        username_lbl = ttk.Label(username_frame, text="Nombre de usuario:", width=25)
+        username_lbl.pack(side="left")
+
+        username_entry = ttk.Entry(username_frame)
+        username_entry.pack(fill="x")
+
+        # Email
+        email_frame = ttk.Frame(form_frame)
+        email_frame.pack(pady=10)
+
+        email_lbl = ttk.Label(email_frame, text="Correo electrónico:", width=25)
+        email_lbl.pack(side="left")
+
+        email_entry = ttk.Entry(email_frame)
+        email_entry.pack(fill="x")
+
+
+        # Navigation buttons
+        buttons_frame = tk.Frame(self)
+        buttons_frame.pack(pady=25)
+
+        next_player = ttk.Button(buttons_frame, text="Siguiente jugador")
+        next_player.pack(side="left")
+
+        return_btn = ttk.Button(buttons_frame, text="Volver", command=lambda:controller.show_frame(UsrRegisteredSelection))
+        return_btn.pack(padx=10)
+
+class MenuData(tk.Frame):
+    """
+    Clase que crea los elementos del menú de deciciones respecto al registro de usuarios del juego.
+    """
+    def __init__(self, parent, controller):
+        """
+        Constructor de las vistas de menú de selección de visualización de datos del juego.
+        :param parent: Clase de la que hereda el contenedor.
+        :param controller: Clase de la que hereda las funciones de control.
+        """
+        tk.Frame.__init__(self, parent)
+
+        # Title
+        title = ttk.Label(self, text="¿Qué información desea consultar?", font=NORMAL_FONT)
+        title.pack(pady=10)
+
+        # Navigation Buttons
+        buttons_frame = tk.Frame(self)
+        buttons_frame.pack(pady=15)
+
+        players = ttk.Button(buttons_frame, text="Jugadores", command=lambda: controller.show_frame(PlayerData))
+        players.pack(pady=10)
+
+        themes = ttk.Button(buttons_frame, text="Temas", command=lambda: controller.show_frame(ThemeData))
+        themes.pack(pady=15)
+
+        return_btn = ttk.Button(buttons_frame, text="Volver", command=lambda: controller.show_frame(StartPage))
+        return_btn.pack(pady=15)
+
+class ThemeData(tk.Frame):
+    """
+    Clase que crea los elementos del menú de deciciones respecto al registro de usuarios del juego.
+    """
+    def __init__(self, parent, controller):
+        """
+        Constructor de las vistas de los temas almacenados del juego.
+        :param parent: Clase de la que hereda el contenedor.
+        :param controller: Clase de la que hereda las funciones de control.
+        """
+        tk.Frame.__init__(self, parent)
+
+        # Title
+        title = ttk.Label(self, text="¡Temas!", font=LARGE_FONT)
+        title.pack(pady=10)
+
+        # Input frame
+        input_frame = tk.Frame(self)
+        input_frame.pack(pady=15)
+
+        theme_lbl = ttk.Label(input_frame, text="Seleccione un tema:", width=20)
+        theme_lbl.pack(side="left", padx=10)
+
+        theme_combo = ttk.Combobox(input_frame, values=["One", "Two", "Three"])
+        theme_combo.pack(fill="x")
+
+        # Text area
+        words_frame = tk.Frame(self)
+        words_frame.pack(pady=10)
+
+        words_area = tk.Text(words_frame, width=35, height=8)
+        words_area.pack(fill="x", ipadx=5, ipady=5)
+
+        # Buttons
+        buttons_frame = tk.Frame(self)
+        buttons_frame.pack()
+
+        edit_btn = ttk.Button(buttons_frame, text="Editar")
+        edit_btn.pack(side="left")
+
+        return_btn = ttk.Button(buttons_frame, text="Volver", command=lambda: controller.show_frame(MenuData))
+        return_btn.pack(padx=10)
+
+class PlayerData(tk.Frame):
+    """
+    Clase que crea los elementos del menú de deciciones respecto al registro de usuarios del juego.
+    """
+    def __init__(self, parent, controller):
+        """
+        Constructor de las vistas de los jugadores registrados del juego.
+        :param parent: Clase de la que hereda el contenedor.
+        :param controller: Clase de la que hereda las funciones de control.
+        """
+        tk.Frame.__init__(self, parent)
+
+        # Title
+        title = ttk.Label(self, text="¡Jugadores!", font=LARGE_FONT)
+        title.pack(pady=10)
+
+        # Input
+        input_frame = tk.Frame(self)
+        input_frame.pack(pady=15)
+
+        player_label = ttk.Label(input_frame, text="Seleccione un jugador:", width=20)
+        player_label.pack(side="left")
+
+        player_entry = ttk.Combobox(input_frame, values=["One", "Two", "Three"])
+        player_entry.pack(fill="x", padx=10)
+
+        # Text area
+        words_frame = tk.Frame(self)
+        words_frame.pack(pady=10)
+
+        words_area = tk.Text(words_frame, width=35, height=8)
+        words_area.pack(fill="x", ipadx=5, ipady=5)
+
+        # Buttons
+        buttons_frame = tk.Frame(self)
+        buttons_frame.pack()
+
+        edit_btn = ttk.Button(buttons_frame, text="Editar")
+        edit_btn.pack(side="left")
+
+        return_btn = ttk.Button(buttons_frame, text="Volver", command=lambda: controller.show_frame(MenuData))
+        return_btn.pack(padx=10)
+
+class OnGame(tk.Frame):
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
 
         # Title
-        title = ttk.Label(self, text="USER NOT REGISTERED")
-        title.pack()
+        title = ttk.Label(self, text="Palabras Encadenadas", font=LARGE_FONT)
+        title.pack(pady=10)
 
-class MenuData(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, controller)
+        # Labels
+        # Turn frame
+        turn_frame = tk.Frame(self)
+        turn_frame.pack(pady=15)
 
-class ThemeData(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, controller)
+        turn_lbl = ttk.Label(turn_frame,  text="Es el turno de:", width=15)
+        turn_lbl.pack(side="left")
 
-class PlayerData(tk.Frame):
-    def __init__(self, parent, controller):
-        tk.Frame.__init__(self, controller)
+        player_lbl = ttk.Label(turn_frame, text="Jugador_Seleccionado")
+        player_lbl.pack(fill="x")
+
+        # Themes frame
+        theme_frame = tk.Frame(self)
+        theme_frame.pack()
+
+        theme_lbl = ttk.Label(theme_frame, text="Temas en juego:", width=15)
+        theme_lbl.pack(side="left")
+
+        theme_ongame = ttk.Label(theme_frame, text="Temas_en_juego")
+        theme_ongame.pack(fill="x")
+
+        # Buttons
+        buttons_frame = tk.Frame(self)
+        buttons_frame.pack(pady=25)
+
+        validate_btn = ttk.Button(buttons_frame, text="Validar palabra")
+        validate_btn.pack(side="left")
+
+        surrender_btn = ttk.Button(buttons_frame, text="Rendirse")
+        surrender_btn.pack(padx=10)
+
